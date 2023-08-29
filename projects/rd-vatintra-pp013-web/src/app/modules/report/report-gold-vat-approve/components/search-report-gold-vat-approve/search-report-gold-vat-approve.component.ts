bba8320b864fcfec3ec7d6a21f6a9620b47afa1f
JaruwanWai22 <jaruwanwai.may@gmail.com>
2023-08-29 14:48:44 +0700

    Initial commit

---
+++ projects/rd-vatintra-pp013-web/src/app/modules/report/report-gold-vat-approve/components/search-report-gold-vat-approve/search-report-gold-vat-approve.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search-report-gold-vat-approve',
+  templateUrl: './search-report-gold-vat-approve.component.html',
+  styleUrls: ['./search-report-gold-vat-approve.component.scss']
+})
+export class SearchReportGoldVatApproveComponent implements OnInit {
+
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+  }
+
+
+}
